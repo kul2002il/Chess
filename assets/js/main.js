0e15382d623d83f931e77c1234743c93e9435528
Кулманаков Илья Владимирович <gr482_kiv@ttit.local>
2021-04-05 18:58:06 +0700

    Системные сообщения на половину готовы.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,43 @@
 
-let data = {
-	text: "Hello",
+class Incrementer{
+	constructor(){
+		this.counter = 0;
+	}
+	get(){
+		return this.counter++;
+	}
 }
+
+let id = new Incrementer();
+
+let data = {
+	messages: [
+		{
+			text: "Hello",
+			color: "green",
+			id: id.get()
+		},
+	],
+};
+
+Vue.component("message",{
+	props: ["message"],
+	template: document.getElementById("messageTemplate").innerHTML,
+	computed:
+	{
+		styleObject(){
+			return{
+				border: "1px solid " + this.message.color,
+			}
+		}
+	},
+	method:
+	{
+		close(){
+			data.messages.splice(data.messages.indexOf(this.messages),1);
+		},
+	}
+});
 
 let app = new Vue({
 	el: "#app",
